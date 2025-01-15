783fa45a5e1db2f7b4bc8e2d8f62fd35a047184e
Dusan Peric <dushanperic1@gmail.com>
2025-01-15 22:16:02 +0100

    Add deploy.yml buildspec

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,26 @@
-// @ts-check
+// @ts-nocheck
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
+import { loadEnv } from 'vite';
 
-// https://astro.build/config
+const { REPO_NAME, LIVE_URL, LOCALHOST_URL } = loadEnv(
+  process.env.NODE_ENV,
+  process.cwd(),
+  ''
+);
+
+console.log({ REPO_NAME, LIVE_URL, LOCALHOST_URL });
+
+const SCRIPT = process.env.npm_lifecycle_script || '';
+const isBuild = SCRIPT.includes('astro build');
+let BASE_URL = LOCALHOST_URL;
+
+if (isBuild) {
+  BASE_URL = LIVE_URL;
+}
+
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: BASE_URL,
+  ...(isBuild && { base: REPO_NAME }),
 });
